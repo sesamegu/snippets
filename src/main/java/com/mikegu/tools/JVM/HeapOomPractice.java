9b0041705a0468498b8b825a4fcefbd566ed34c1
guhaiquan <guhaiquan@u51.com>
2018-06-21 19:43:15 +0800

    add heap example

--- src/main/java/com/mikegu/tools/JVM/HeapOomPractice.java
+++ src/main/java/com/mikegu/tools/JVM/HeapOomPractice.java
@@ -1,38 +1,44 @@
 package com.mikegu.tools.JVM;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 类说明:
+ * 类说明: 测试heap区发生OOM
+ * 为了快速发生问题，设置启动参数: -Xms256M  -Xmx256M  -XX:+HeapDumpOnOutOfMemoryError  -XX:-UseGCOverheadLimit
+ *
  *
  * @author guhaiquan 2018/6/21
  */
 public class HeapOomPractice {
 
+    private List<AnObject> objects = new ArrayList<>();
 
-    public void play(){
 
+    public static void main(String[] argv) {
 
+        Thread myThread = new Thread(() -> new HeapOomPractice().play());
+        myThread.setName("My thread");
+        myThread.start();
+    }
+
+    public void play() {
+        String data = "123232312333333333333333333333333333333";
+        for (int i = 0; ; i++) {
+            objects.add(new AnObject(data + i));
+        }
     }
 
 
+    class AnObject {
 
+        private String filedOne;
 
-    public static void main(String[] argv){
-//        new Thread(()->new HeapOomPractice().play());
-
+        public AnObject(String filedOne) {
+            this.filedOne = filedOne;
+        }
     }
-
-
-
-
-
 
 }
 
 
-class AnObject{
-    private String filedOne;
-
-    public AnObject(String filedOne) {
-        this.filedOne = filedOne;
-    }
-}
