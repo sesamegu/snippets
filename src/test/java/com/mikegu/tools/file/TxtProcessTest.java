7110aaccc4cbccf90a60b97f3956d87119094771
haiquan.guhq <haiquan.guhq@alibaba-inc.com>
2022-03-18 19:50:37 +0800

    add unit test

---
+++ src/test/java/com/mikegu/tools/file/TxtProcessTest.java
@@ -0,0 +1,12 @@
+package com.mikegu.tools.file;
+
+import org.junit.jupiter.api.DisplayName;
+
+class TxtProcessTest {
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("hihi")
+    void readFileByLines() {
+        System.out.println("hi");
+    }
+}
