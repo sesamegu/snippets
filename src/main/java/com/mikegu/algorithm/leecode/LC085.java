e7e7a1dbb2ada07d9e7bcdbada7a27d147b7f654
haiquan.guhq <haiquan.guhq@alibaba-inc.com>
2023-04-21 19:38:43 +0800

    add LC

---
+++ src/main/java/com/mikegu/algorithm/leecode/LC085.java
@@ -0,0 +1,49 @@
+package com.mikegu.algorithm.leecode;
+
+/**
+ * Introduction:
+ *
+ * @author sesame 2023/4/20
+ */
+public class LC085 {
+
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length;
+        int[][] left = new int[m][n];
+
+        //按每行的初始化 连续的值
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                //无效右下角
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                int width = left[i][j];
+                int area = width;
+                for (int k = i - 1; k >= 0; k--) {
+                    //宽度
+                    width = Math.min(width, left[k][j]);
+                    //高度递增
+                    area = Math.max(area, (i - k + 1) * width);
+                }
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+
+}
